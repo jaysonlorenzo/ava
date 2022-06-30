6e9659c151c7c61cfc8d695662d6703dab4d0fb9
jaysonlorenzo <jayson.lorenzo2577@gmail.com>
2022-06-30 17:33:11 +0800

    Deleted some stuff

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -9,7 +9,18 @@ const Pricing = () => {
         <div>
             <div>
                 <h2>Pricing</h2>
+                <h3></h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consectetur ligula sit amet magna cursus condimentum. Nunc tempus arcu porttitor porttitor lacinia. Cras non magna quam.</p>
+
+            </div>
+            <div>
+
+            </div>
+            <div>
+                <span>Standard</span>
+                <div>
+                    <p>$49</p>
+                </div>
             </div>
         </div>
     </div>
